d875b18d269493aa0917850f96a2f7d01639a38c
ensar643 <ensar643@BPC01>
2024-02-15 09:08:24 +0300

    Proje dosyası ekle.

---
+++ KitapEvi/Models/KitapKategori.cs
@@ -0,0 +1,9 @@
+﻿namespace KitapEvi.Models
+{
+    public class KitapKategori
+    {
+        public int kategoriId { get; set; }
+        public string kategoriName { get; set; }
+        List<KitapModel>kitapListesi = new List<KitapModel>();
+    }
+}
